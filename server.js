25b08c386a1db3f03caaaad31d98e6fdd68a93a9
Bogning Rodney <bogningrodney2@gmail.com>
2026-07-07 19:52:24 +0100

    cors policy fixed

--- server.js
+++ server.js
@@ -17,10 +17,15 @@ app.use(express.json());
 // ─────────────────────────────────────────────
 // Middlewares globaux
 // ─────────────────────────────────────────────
-app.use(cors({
-  origin: "http://localhost:5173" || 'http://localhost:3000' ,
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: "http://localhost:5173" ,
+//   credentials: true,
+// }));
+// ─────────────────────────────────────────────
+// Middlewares globaux
+// ─────────────────────────────────────────────
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
